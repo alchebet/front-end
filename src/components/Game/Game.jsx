ff7087467731cdf43a120ff433ec0a33381971f9
cficht <chris.ficht@gmail.com>
2020-06-26 11:41:49 -0700

    conditional answer stuff

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { getGame } from '../../services/game';
+import { getGame, patchAnswer } from '../../services/game';
 import { useUser } from '../hooks/Provider'
 
 export default function Game() {
   const [game, setGame] = useState({});
+  const [answer, setAnswer] = useState('');
   const [isCreator, setIsCreator] = useState(false);
   const user = useUser();
   const {id} = useParams();
@@ -18,11 +19,44 @@ export default function Game() {
     if(game.creator === user._id) setIsCreator(true)
   }, [game, user])
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    patchAnswer(id, {answer: answer})
+    .then(() => alert('Answer submitted!'))
+    .then(() => window.location.reload())
+  }
+
+  const isAnswer = () => {
+    if(game.answer?.length < 1)
+    return (
+    <>
+      <input type="text" placeholder="Write an answer" value={answer} onChange={(e) => setAnswer(e.target.value)}/>
+      <button onClick={handleSubmit}>Submit Answer</button>
+    </>
+    )
+    else return (
+    <h3>{game.answer}</h3>
+    )
+  }
+
+  const creatorInfo = () => {
+    if(isCreator)
+    return (
+      <div>
+      {isAnswer()}
+      </div>
+    )
+    else return (
+      <h3>You didn't make this.</h3>
+    )
+  }
+
   return (
     <div>
       <h1>Game Details</h1>
       <h2>{game.title}</h2>
       <h3>{game.description}</h3>
+      {creatorInfo()}
     </div>
   )
 }
--- src/services/game.js
+++ src/services/game.js
@@ -1,4 +1,4 @@
-import { post, get } from "./request";
+import { post, get, patch } from "./request";
 
 export const postGame = game => post('/api/v1/game', game);
 
@@ -7,4 +7,8 @@ export const getGames = () => get('/api/v1/game');
 export const getGame = (id) => {
   console.log(id)
   return get(`/api/v1/game/${id}`);
+}
+
+export const patchAnswer = (id, answer) => {
+  return patch(`/api/v1/game/${id}`, answer)
 }
