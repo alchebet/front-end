d0dd93bea3caf608d99281f60ef1fcd92caa3bee
Jaime Sanders <jaimelyn517@gmail.com>
2020-06-26 16:53:17 -0700

    log out button

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,9 +2,19 @@ import React, { useState } from 'react'
 import { useUser } from '../hooks/Provider'
 import CreateGame from '../Game/CreateGame';
 import ListOfGames from '../Game/ListOfGames';
+import { logout } from '../../services/auth';
+import { useHistory } from 'react-router-dom';
 
 export default function Dashboard() {
  const user = useUser();
+ const history = useHistory();
+
+ const handleLogOut = () => {
+   console.log('logout')
+   logout()
+   .then(() => history.push('/'))
+ }
+
  console.log(user)
 
   return (
@@ -12,6 +22,7 @@ export default function Dashboard() {
       <h1>{user.displayName}'s Dashboard</h1>
       <CreateGame />
       <ListOfGames />
+      <button onClick={handleLogOut}>Sign Out</button>
     </div>
   )
 }
--- src/services/auth.js
+++ src/services/auth.js
@@ -2,4 +2,5 @@ import { post, get } from "./request";
 
 export const signup = user => post('/api/v1/users/signup', user);
 export const login = user => post('/api/v1/users/login', user);
-export const verify = () => get('/api/v1/users/verify')
+export const verify = () => get('/api/v1/users/verify');
+export const logout = () => get('/api/v1/users/logout');
